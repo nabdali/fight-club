3445fdc8fd6b91b253d9cc4449240a0d33dda72a
theoGandon <theo.gandon9@gmail.com>
2026-04-16 17:27:21 +0200

    Nettoyage gradle

--- character-service/src/test/java/fightclub/characterservice/CharacterServiceApplicationTests.java
+++ character-service/src/test/java/fightclub/characterservice/CharacterServiceApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class CharacterServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
--- character-service/src/test/java/fightclub/characterservice/TestCharacterServiceApplication.java
+++ character-service/src/test/java/fightclub/characterservice/TestCharacterServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestCharacterServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.from(CharacterServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.from(CharacterServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
 
 }
