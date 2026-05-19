72e6b791abe029cf929f9b579f299af5e51b0a33
Joseph Baert <145338459+Sephoyo@users.noreply.github.com>
2026-05-19 13:56:59 +0200

    add entity and repository for scoreboard (#22)
    * add entity and repository for scoreboard
    
    # Conflicts:
    #	.idea/vcs.xml
    #	leaderboard-service/pom.xml
    #	leaderboard-service/src/main/resources/application.yaml
    
    * rebase :)
    
    * fix: delete old character service
    
    * feat: refactor AppBoardEntity to UserStatistic and update repository
    
    - feat : create table user statistic
    
    * Update leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
    
    Co-authored-by: Ali Boulajine <abdelaliboulajine@gmail.com>
    
    ---------
    
    Co-authored-by: Ali B. <abdelaliboulajine@gmail.com>

---
+++ leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
@@ -0,0 +1,9 @@
+package com.figth_club.leaderboard_service.repositories;
+
+import com.figth_club.leaderboard_service.entities.UserStatistic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppBoardRepository extends JpaRepository<UserStatistic, Integer> {
+}
---
+++ leaderboard-service/src/main/java/com/figth_club/leaderboard_service/services/AppBoardService.java
@@ -0,0 +1,7 @@
+package com.figth_club.leaderboard_service.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppBoardService{
+}
