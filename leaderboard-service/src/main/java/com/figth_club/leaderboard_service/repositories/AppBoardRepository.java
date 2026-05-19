92354f2324a42bc974ca42f86fb918193ebb33e6
Johari-ops <joseph.baert@student.junia.com>
2026-05-19 15:23:28 +0200

    end point basic

--- leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
+++ leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
@@ -4,6 +4,17 @@ import com.figth_club.leaderboard_service.entities.UserStatistic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AppBoardRepository extends JpaRepository<UserStatistic, Integer> {
+    List<UserStatistic> findAllByOrderByVictoryCounterDesc();
+
+    List<UserStatistic> findAllByOrderByDefeatCounterDesc();
+
+    List<UserStatistic> findAllByIdCharacterOrderByVictoryCounterDesc(Integer idCharacter);
+
+    List<UserStatistic> findAllByIdCharacterOrderByDefeatCounterDesc(Integer idCharacter);
+
+
 }
