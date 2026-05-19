076c12d0d96bb198175647f196998067a2340984
Joseph Baert <145338459+Sephoyo@users.noreply.github.com>
2026-05-19 13:56:27 +0200

    Update leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
    Co-authored-by: Ali Boulajine <abdelaliboulajine@gmail.com>

--- leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
+++ leaderboard-service/src/main/java/com/figth_club/leaderboard_service/repositories/AppBoardRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppBoardRepository extends JpaRepository<UserStatistic, Long> {
+public interface AppBoardRepository extends JpaRepository<UserStatistic, Integer> {
 }
