1d3efbf4811fc71a8ac10985de4385cd5dba27fc
Johari-ops <joseph.baert@student.junia.com>
2026-05-19 15:23:28 +0200

    end point basic

--- leaderboard-service/src/main/java/com/figth_club/leaderboard_service/controllers/BoardController.java
+++ leaderboard-service/src/main/java/com/figth_club/leaderboard_service/controllers/BoardController.java
@@ -1,14 +1,51 @@
 package com.figth_club.leaderboard_service.controllers;
 
+import com.figth_club.leaderboard_service.entities.UserStatistic;
+import com.figth_club.leaderboard_service.services.AppBoardService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/api")
 public class BoardController {
 
-    @RequestMapping("/leaderboard")
-    public String infos(){
-        return "leaderboard";
+    private final AppBoardService appBoardService;
+
+    public BoardController(AppBoardService appBoardService) {
+        this.appBoardService = appBoardService;
     }
+
+    @RequestMapping("/leaderboard")
+    public List<UserStatistic> getStats(){
+        return appBoardService.getAllStatistics();
+    }
+
+    @PostMapping("/create")
+    public UserStatistic createStats() {
+        return appBoardService.createStatistic();
+    }
+
+    @GetMapping("/most-victories")
+    public List<UserStatistic> getMostVictories() {
+        return appBoardService.getLeaderboardByVictories();
+    }
+
+    @GetMapping("/most-defeats")
+    public List<UserStatistic> getMostDefeats() {
+        return appBoardService.getLeaderboardByDefeats();
+    }
+    @GetMapping("/most-defeats-character/id")
+    public List<UserStatistic> getMostDefeatsByIdC(@RequestParam Integer id) {
+        return appBoardService.getLeaderboardByCharacterByDefeats(id);
+    }
+    @GetMapping("/most-victories-character/id")
+    public List<UserStatistic> getMostVictoriesByIdC(@RequestParam Integer id) {
+        return appBoardService.getLeaderboardByCharacterByVictories(id);
+    }
+
 }
