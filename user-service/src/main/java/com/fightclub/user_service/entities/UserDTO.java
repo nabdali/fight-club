2a3be4bfe72000d33c920ae4ec3a19b506a6446a
Arthur <arthur.duval18@gmail.com>
2026-05-19 17:13:01 +0200

    feat(user_service): add enpoint login

---
+++ user-service/src/main/java/com/fightclub/user_service/entities/UserDTO.java
@@ -0,0 +1,21 @@
+package com.fightclub.user_service.entities;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class UserDTO {
+
+    @NotBlank(message = "Email obligatoire")
+    @Email(message = " Format de l'email invalide")
+    private String email;
+
+    @NotBlank(message = "Le pseudo est obligatoire")
+    private String pseudo;
+
+}
