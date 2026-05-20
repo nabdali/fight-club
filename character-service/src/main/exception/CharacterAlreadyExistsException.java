2f3af3f264206cc00110cc0da1fcb7b0e47c6686
TheoGandon <theo.gandon9@gmail.com>
2026-05-20 13:39:36 +0200

    Feat/character service fix file of service

--- character-service/src/main/exception/CharacterAlreadyExistsException.java
+++
@@ -1,5 +0,0 @@
-package com.fightclub.character_service.exception;
-
-public class CharacterAlreadyExistsException extends RuntimeException {
-    public CharacterAlreadyExistsException(String message) { super(message); }
-}
--- character-service/src/main/exception/CharacterTypeNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.fightclub.character_service.exception;
-
-public class CharacterTypeNotFoundException extends RuntimeException {
-    public CharacterTypeNotFoundException(String message) { super(message); }
-}
